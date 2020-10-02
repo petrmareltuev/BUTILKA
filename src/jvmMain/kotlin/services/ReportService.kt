20286d84293cd736bf9bb9a449ea20a12ed425f1
petrmareltuev <mareltuev.petr@rambler.ru>
2020-10-02 15:20:18 +0300

    report fix

--- src/jvmMain/kotlin/services/ReportService.kt
+++ src/jvmMain/kotlin/services/ReportService.kt
@@ -1,6 +1,7 @@
 package services
 
 import database.*
+import database.Lohs.passportSerialNumber
 import model.Report
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.insert
@@ -9,21 +10,20 @@ import org.jetbrains.exposed.sql.select
 
 actual class ReportService{
     actual suspend fun sendReport(report: Report): String {
-        database{
-            Lohs.select { Lohs.passportSerialNumber eq report.victim }
-        }.firstOrNull()?.let{
-            println(Lohs.passportSerialNumber)
-            val loh = Lohs.fullname
-            Reports.insert {
-                it[caseNumber] = report.case_number
-                it[participants] = report.participants
-                it[victim] = report.victim
-                it[witness] = report.witness
-                it[comment] = report.comment
-                it[lohName] = loh
-            }
-        }.let{
-            return "OK"
+        return database{
+            Lohs.select { Lohs.passportSerialNumber eq report.victim }.firstOrNull()?.let{
+                println(it[passportSerialNumber])
+                val loh = it[Lohs.fullname]
+                Reports.insert {
+                    it[caseNumber] = report.case_number
+                    it[participants] = report.participants
+                    it[victim] = report.victim
+                    it[witness] = report.witness
+                    it[comment] = report.comment
+                    it[lohName] = loh
+                }.let{ "OK"}
+            }?: "Not OK"
+
         }
     }
 }
